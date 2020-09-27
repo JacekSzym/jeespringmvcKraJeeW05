9e490a13816566e1a21fafe724c2feaa023f3e23
Rafal <rafal.opial@coderslab.pl>
2020-09-27 14:58:54 +0200

    M5D4TxZ2 pass attribute through model

--- src/main/java/pl/coderslab/jeespringmvc/controller/HelloController.java
+++ src/main/java/pl/coderslab/jeespringmvc/controller/HelloController.java
@@ -1,8 +1,11 @@
 package pl.coderslab.jeespringmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalTime;
 
 @Controller
 public class HelloController {
@@ -21,5 +24,23 @@ public class HelloController {
     @GetMapping("/helloview")
     public String helloView() {
         return "hello";
+    }
+
+    @GetMapping("/home")
+    public String home(Model m) {
+        String background = "white";
+        String textColor = "black";
+
+        final int currentHour = LocalTime.now().getHour();
+
+        if(currentHour > 19 || currentHour < 8){
+            background = "black";
+            textColor = "white";
+        }
+
+        m.addAttribute("backgroundColor", background);
+        m.addAttribute("textColor", textColor);
+
+        return "home";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Edu
+  Date: 27.09.2020
+  Time: 14:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>home</title>
+    <style>
+        body {
+            background-color: ${backgroundColor};
+            color: ${textColor};
+        }
+    </style>
+</head>
+<body>
+home.jsp
+</body>
+</html>
