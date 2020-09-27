a7e491d5087561d4cc9e337b457c2d13af0c83e7
Rafal <rafal.opial@coderslab.pl>
2020-09-27 11:05:25 +0200

    M5D4T1Z2 pierwszy kontroler

---
+++ src/main/java/pl/coderslab/jeespringmvc/controller/HelloController.java
@@ -0,0 +1,20 @@
+package pl.coderslab.jeespringmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/hellojsp")
+    public String helloJsp() {
+        return "index.jsp";
+    }
+}
